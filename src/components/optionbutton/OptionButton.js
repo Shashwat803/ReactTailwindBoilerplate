cd8368613b3871c5d692a321b6760f13a54e4d98
ExerticaShashwat <exertica.shashwat@outlook.com>
2024-04-04 01:22:08 +0530

    Fixed pagethree and option button

--- src/components/optionbutton/OptionButton.js
+++ src/components/optionbutton/OptionButton.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const OptionButton = ({ option,  SetOptionSelected }) => {
   return (
-    <div id="options" className="  w-3/4 md:w-[570px]">
+    <div id="options" className="  w-[90%] md:w-[570px]">
       <button onClick={()=> SetOptionSelected(true)} className=" flex items-center border mt-2 rounded-md w-full p-4 focus:ring-1 ring-yellow-400 hover:shadow-lg ">
         <img src={option.imagePath} className=" w-7 h-7 " alt="" />
-        <div className=" ml-4 float-left text-sm md:text-md">{option.option}</div>
+        <div className=" ml-4 float-left text-xs md:text-md">{option.option}</div>
       </button>
       </div>
   )
--- src/pages/PageThree.js
+++ src/pages/PageThree.js
@@ -6,12 +6,12 @@ const PageThird = () => {
   const [active, setActive] = useState(false)
   return (
 <div className='flex flex-col items-center justify-start h-[100%]'>
-<div className=' flex flex-col md:flex-row items-center justify-center h-[70vh]'>
+<div className=' flex flex-col md:flex-row items-center justify-center h-[60vh]'>
     <div id="video" className=' w-1/2 md:h-2/3'>
     <video src='https://brilliant.org/site_media/version-0/images/nux-animations/pulley-blob.mp4' autoPlay loop className='w-full h-full' >
     </video>
     </div>
-    <div id="info" className=' mt-4 md:mt-0 w-[85%] md:w-3/5 flex items-center justify-center flex-col md:block'>
+    <div id="info" className=' mt-8 md:mt-0 w-[85%] md:w-3/5 flex items-center justify-center flex-col md:block'>
         <div id="head" className='  text-xl md:text-3xl font-bold'>You're in the right place</div>
         <div id="paragraph" className='mt-4 w-full  md:w-2/3 text-sm md:text-md text-center md:text-left '>Brilliant gets you hands-on to help improve your professional
         skills and knowledge. You'll interact with concepts and solve fun problems in math, science, and computer science.</div>
